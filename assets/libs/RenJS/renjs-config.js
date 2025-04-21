5ca20ff820369efb898d77e4ff48949160a2df75
ElricStormking <elric.tw@gmail.com>
2025-04-21 08:02:13 +0800

    Add files via upload

---
+++ assets/libs/RenJS/renjs-config.js
@@ -0,0 +1,11 @@
+RenJSConfig = {
+    startScene: "Start",
+    assetsPath: "./assets/libs/RenJS/RJ_characters",
+    textsPath: "./assets/libs/RenJS/RJ_levelStory",
+    imagesPath: "./assets/libs/RenJS/RJ_backgrounds",
+    soundsPath: "./assets/libs/RenJS/RJ_sounds",
+    background: "./assets/libs/RenJS/RJ_backgrounds",
+    width: 1280,
+    height: 720,
+    plugins: []
+} 
